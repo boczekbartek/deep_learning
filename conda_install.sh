0c6135b17c0bef3d3d17fbbe0488e6f7c66ff7f4
Bartłomiej Boczek <bartek1boczek@gmail.com>
2021-12-09 15:11:55 +0100

    Added code-formatting git hook with pre-commit

--- conda_install.sh
+++ conda_install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-conda install --yes --file requirements/conda_requirements.txt
+conda install --yes --file requirements/requirements.txt
 conda install --yes --file requirements/conda_torch.txt -c pytorch
 pip install -r requirements/only_pip_requirements.txt
