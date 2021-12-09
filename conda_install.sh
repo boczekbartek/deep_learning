eab7bc597962d28e8055c8a462c0007b00697274
Bartłomiej Boczek <bartek1boczek@gmail.com>
2021-12-09 18:08:18 +0100

    Install pre-commit by default

--- conda_install.sh
+++ conda_install.sh
@@ -4,3 +4,5 @@ set -e
 conda install --yes --file requirements/requirements.txt
 conda install --yes --file requirements/conda_torch.txt -c pytorch
 pip install -r requirements/only_pip_requirements.txt
+
+pre-commit install
--- pip_install.sh
+++ pip_install.sh
@@ -4,3 +4,5 @@ set -e
 pip install -r requirements/requirements.txt
 pip install -r requirements/only_pip_requirements.txt
 pip install -r requirements/pip_torch.txt
+
+pre-commit install
